5a46d6acaa374be1a0f85c025d722855c41bf939
Darnisha Patel <darnisha2910@gmail.com>
2025-03-16 00:01:05 +0530

    Modified decimal to binary

--- NumberSystem/decimalToBinary.cpp
+++ NumberSystem/decimalToBinary.cpp
@@ -2,18 +2,24 @@
 #include<cmath>
 using namespace std;
 
-void decimalToBinary(int n) {
+int decimalToBinary(int n) {
     int bit = 0;
+    int i = 0;
+    int binaryno = 0;
     while(n>0){
         bit = n%2;
-        cout<<bit<<endl;
+      binaryno =bit*pow(10,i++)+ binaryno;
+        // cout<<bit<<endl;
         n = n/2;
     }
+    return binaryno;
 }
 
 int main() {
     int n;
     cout<<"Enter the number You want to convert in Binary :: "<<endl;
     cin>>n;
-    decimalToBinary(n);
+    cout<<endl;
+    int binary  = decimalToBinary(n);
+    cout<<binary;
 }
