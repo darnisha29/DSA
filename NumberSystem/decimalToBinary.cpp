40b4376552ad36321afc53aa2c376a6f0f51b5e0
Darnisha Patel <darnisha2910@gmail.com>
2025-03-16 18:33:46 +0530

    Implementd Bitwise Methode For Decimal to Binary Conversion

--- NumberSystem/decimalToBinary.cpp
+++ NumberSystem/decimalToBinary.cpp
@@ -2,12 +2,36 @@
 #include<cmath>
 using namespace std;
 
+// Division method
 int decimalToBinary(int n) {
     int bit = 0;
     int i = 0;
     int binaryno = 0;
     while(n>0){
         bit = n%2;
+      binaryno =bit*pow(10,i++)+ binaryno;
+        // cout<<bit<<endl;
+        n = n/2;
+    }
+    return binaryno;
+}
+
+int main() {
+    int n;
+    cout<<"Enter the number You want to convert in Binary :: "<<endl;
+    cin>>n;
+    cout<<endl;
+    int binary  = decimalToBinary(n);
+    cout<<binary;
+}
+
+// Bitwise 
+int decimalToBinary(int n) {
+    int bit = 0;
+    int i = 0;
+    int binaryno = 0;
+    while(n>0){
+        bit = n & 1;
       binaryno =bit*pow(10,i++)+ binaryno;
         // cout<<bit<<endl;
         n = n/2;
