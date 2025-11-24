c8d928de8e75c66f3de8a19260ee654ec2e6f0d9
Darnisha Patel <darnisha2910@gmail.com>
2025-11-24 21:52:47 +0530

    Implement Remove all occurence of substring

---
+++ CharArr and Strings/removeAllOccurence.cpp
@@ -0,0 +1,17 @@
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    string removeOccurrences(string s, string part)
+    {
+        int pos = s.find(part);
+        while (pos != string::npos)
+        {
+            s.erase(pos, part.length());
+            pos = s.find(part);
+        }
+        return s;
+    }
+};
